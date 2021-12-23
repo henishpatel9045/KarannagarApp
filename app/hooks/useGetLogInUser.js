e6ddd54aaf34d50e0f92a0d8777693a4b0ca7fb8
henishpatel9045 <200120107182@git.org.in>
2021-12-23 17:25:36 +0530

    Loading animation added in login screen and code refactored in login hook.

--- app/hooks/useGetLogInUser.js
+++ app/hooks/useGetLogInUser.js
@@ -1,24 +1,29 @@
 import { useContext, useState } from "react";
+import { isUserRegistered } from "../api/firebase";
 import AuthContext from "../auth/context";
 import useStorage from "../auth/useStorage";
 import useGoogleLogIn from "./useGoogleLogIn";
 
 export default (setFunction) => {
   const { setcurrUser } = useContext(AuthContext);
-  const { logIn, fetchUserInfo } = useGoogleLogIn();
+  const { logIn, fetchUserInfo, loading } = useGoogleLogIn();
   const [error, setError] = useState();
 
   const authPopUp = () => {
     logIn().then((res) => {
       if (res.error) setError(res.errorCode);
       else if (res.type === "success") {
-        fetchUserInfo(res.authentication.accessToken).then((res) => {
-          setFunction(res);
-          setcurrUser(res);
+        fetchUserInfo(res.authentication.accessToken).then((user) => {
+          isUserRegistered(user.email).then((exists) => {
+            if (exists) {
+              setFunction(user);
+              setcurrUser(user);
+            } else setError("User not registered.");
+          });
         });
       }
     });
   };
 
-  return { authPopUp, error };
+  return { authPopUp, error, loading };
 };
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,37 +1,61 @@
-import { Text, StyleSheet, Image, Dimensions } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Dimensions,
+} from "react-native";
 import React from "react";
 import colors from "../configs/colors";
 import SocialMediaIcon from "../components/SocialMediaIcon";
 import Screen from "../components/Screen";
 import useGetLogInUser from "../hooks/useGetLogInUser";
 import useStorage from "../auth/useStorage";
+import LottieView from "lottie-react-native";
 
 export default function LoginScreen({ navigation }) {
   const { setUser, user } = useStorage();
-  const { authPopUp, error } = useGetLogInUser(setUser);
+  const { authPopUp, error, loading } = useGetLogInUser(setUser);
 
   return (
-    <Screen style={styles.container}>
-      <Text style={styles.heading}>Login</Text>
-      <Image
-        source={require("../assets/login.png")}
-        width={100}
-        height={100}
-        style={styles.imagebg}
-      />
-      <SocialMediaIcon
-        name={"google"}
-        style={styles.google}
-        title={"Google LogIn"}
-        onPress={authPopUp}
-      />
-      <SocialMediaIcon
-        name={"facebook"}
-        style={styles.google}
-        title="Facebook LogIn"
-        onPress={authPopUp}
-      />
-    </Screen>
+    <>
+      {loading ? (
+        <LottieView
+          source={require("../assets/animations/loadingSpinner.json")}
+          autoPlay
+          loop
+        />
+      ) : (
+        <Screen style={styles.container}>
+          <Text style={styles.heading}>Login</Text>
+          {error && <Text style={styles.error}>{error}</Text>}
+          <Image
+            source={require("../assets/login.png")}
+            width={100}
+            height={100}
+            style={styles.imagebg}
+          />
+          <SocialMediaIcon
+            name={"google"}
+            style={styles.google}
+            title={"Google LogIn"}
+            onPress={authPopUp}
+          />
+          <SocialMediaIcon
+            name={"facebook"}
+            style={styles.google}
+            title="Facebook LogIn"
+            onPress={authPopUp}
+          />
+          <TouchableOpacity
+            style={styles.footer}
+            onPress={() => navigation.navigate("Register")}
+          >
+            <Text style={styles.txt}>Not a user? Register yourself.</Text>
+          </TouchableOpacity>
+        </Screen>
+      )}
+    </>
   );
 }
 
@@ -50,6 +74,14 @@ const styles = StyleSheet.create({
     textShadowColor: colors.dark,
     textShadowRadius: 3,
   },
+  error: {
+    color: "red",
+    fontSize: 20,
+    top: 60,
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    alignSelf: "center",
+  },
   imagebg: {
     alignSelf: "center",
     height: 300,
@@ -61,5 +93,15 @@ const styles = StyleSheet.create({
     alignSelf: "center",
 
     width: "80%",
+  },
+  footer: {
+    position: "absolute",
+    bottom: 25,
+    alignSelf: "center",
+  },
+  txt: {
+    color: "#4267b2",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
